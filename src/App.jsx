8a5271e2880cfc3130ea65e1ca62c003da1e9909
TahianSylvain <ranjalahyandrytahiansylvain@gmail.com>
2023-09-22 15:23:20 +0300

    Schedule emploi-temps.prof mise en place; besoin design ajustement

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import './homeee.css';
 import './scrollbarrr.css';
 import './carousel2.css';
 import './tea_home.css';
+import './Responsive.css'
+import { Schedule } from './Components/Schedule.jsx';
 import { InteractiveSearch, Clock } from './Components/RepairedInJet';
 import {DeptUL, CourseList, InstitutList} from './Components/UserInterface';
 import { useState, useRef } from 'react';
-
 
 
 function App() {
@@ -22,16 +23,34 @@ function App() {
         }
     };    
     const Nouveau = () => {
-        if (state.enter==="")
+        const date = new Date();
+        // const options = { year: '2-digit', month: '2-digit', day: '2-digit' };
+        // const formattedDate = date.toLocaleDateString('en-GB', options);
+
+        if (state.enter === "")
             return(<></>)
-        else
-            return(<div className="course_list">
-            <a classe="row" href="/url/">
-                <span className="col col1"> {state.enter} </span>
-                <span className="col col2"> {new Date().toLocaleDateString()} </span>
-                <span className="col col3"> now </span>
-            </a>
-        </div>)
+        else // AZA ATAO ANATY ./NewFile.css ireo style={{}} ireo fa potika
+            return(
+            <div className="new--course" style={{
+                            width: "100%",
+                            height: "100%"
+                        }}>
+                <a classe="new--row" href="/url/" style={{
+                            textDecoration: "None",
+                            fontSize: "0.9rem", 
+                            fontFamily: "primary_font",             
+                            display: "flex",
+                            padding: "5px 30px",
+                        }}>
+                    <span style={{ width: "60%",  }}> {state.enter} </span>
+                    <span style={{ width: "20%", textAlign:"center" }}> 
+                        {date.getDay()<10 ? "0"+date.getDay().toString():date.getDay().toString()}-
+                        {date.getMonth()<10 ? "0"+date.getMonth().toString(): date.getMonth().toString()}-
+                        {date.getFullYear().toString().slice(2, )}
+                     </span>
+                    <span style={{ width: "20%", textAlign:"center" }}> now </span>
+                </a>
+            </div>)
     }
     function handleText(e){
         setState({enter: e.target.value})
@@ -52,7 +71,7 @@ function App() {
                 <div className="logo">
                     <div className="logo_img">
                         <img src="./lvlind9.webp" width="40px" height="40px" alt="project" />
-                        <img src="./logo-ISPM.webp" width="40px" height="40px" alt="ispm" />
+                        <img src="./logoISPM.webp" width="40px" height="40px" alt="ispm" />
                     </div>
                 </div>
                 <nav>
@@ -129,7 +148,7 @@ function App() {
                     
                     <div className="stu_header">
                         <div className="topbar_container">
-                            <strong>LevelMind</strong>
+                            <strong id="strong">LevelMind</strong>
                                 <Clock/> 
                             <InteractiveSearch/>
 <svg width="20" height="20" version="1.1" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -150,7 +169,8 @@ function App() {
                     </div>
 
                 </div>
-                <div className="main_content"><div className="tea_block">
+                <div className="main_content">
+    <div className="tea_block">
 
         <main className="courses_container">
             <div className="course_list_container">
@@ -182,7 +202,6 @@ function App() {
                 <h3>Add course</h3>
             </header>
             <form method="post" encType="multipart/form-data">
-                {/* {% csrf_token %} */}
                 <div className="input_group course_title">
                     <label htmlFor="course_title">Title</label>
                     <input type="text" id="course_title" name="title" value={state.enter} onChange={handleText}/>
@@ -211,11 +230,11 @@ function App() {
 <aside className="aside_container">
     <div className="aside_content">
         <div className="emploie-temps">
-            <img src="" width="400" height="500" alt="" id="schedule" />
+            <Schedule></Schedule> 
         </div>
         <header className="aside_header">
             <h4>
-                Todo list ----------------
+                Todo list
 <svg width="18" height="18" version="1.1" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
 <path transform="rotate(-90)" d="m-12.4 8h6.75c0.346 0 0.625 0.446 0.625 1s-0.279 1-0.625 1h-6.75c-0.346 0-0.625-0.446-0.625-1s0.279-1 0.625-1z" fill="#fff"/>
 <path d="m5.75 8h6.5c0.416 0 0.75 0.446 0.75 1s-0.334 1-0.75 1h-6.5c-0.416 0-0.75-0.446-0.75-1s0.334-1 0.75-1z" fill="#fff"/>
--- src/Components/RepairedInJet.jsx
+++ src/Components/RepairedInJet.jsx
@@ -5,7 +5,8 @@ import product from './Course.json'
 export const Clock = (props) => {
     const [now, setNow] = useState(new Date())
     setInterval(()=>setNow(new Date()), 1000)
-    return  <div>{now.toLocaleTimeString()}</div>
+    
+    return  <div id='tim'>{now.toLocaleTimeString()}</div>
 }
 
 
@@ -32,32 +33,3 @@ export const InteractiveSearch = () => {
         />
     </div>
 }
-
-    // const [cat, setCat] = useState({mark: ''})
-    // function handleFilter(e){
-    //         const selectedCategory = e.target.value;
-    //         setCat({ mark: selectedCategory });
-    //         const filteredProducts = product.filter(
-    //             item => item.category === selectedCategory || selectedCategory === "*" // specified or default 
-    //         );
-    //         const items = filteredProducts.map(item => (
-    //             <li key={item.id}>{item.id}
-    //                 <p>
-    //                     {item.name} {item.category} <i>{item.cost}</i>
-    //                 </p>
-    //                 <button type="button" onClick={() => alert('Bought!')}>
-    //                     Buy
-    //                 </button>
-    //             </li>
-    //         ));
-    //         setTarget(items);
-    // }
-    /*
-            <select name="categorifying" id="choose" value={cat.mark} onChange={handleFilter}>
-                <option value="*">Default</option>
-                <option value="meats">meats</option>
-                <option value="fruits">fruits</option>
-                <option value="vegetables">vegetables</option>
-            </select> 
-            <ul>{target}</ul>
-    */
